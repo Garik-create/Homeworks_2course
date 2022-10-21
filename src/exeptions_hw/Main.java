f746c2672e0d1c514da61d0999a333e033810752
Garik-create <pfdfkbybujhm@yandex.ru>
2022-10-21 09:40:56 +0300

    Исключения - д.з.1

--- src/exeptions_hw/Main.java
+++ src/exeptions_hw/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         String login = "w";
         String password = "QWErty220";
-        String confirmPassword = "QWErty220";
+        String confirmPassword = "QWErty22";
 
         if (Data.checkCredentials(login, password, confirmPassword)) {
             System.out.println("Ok!");
